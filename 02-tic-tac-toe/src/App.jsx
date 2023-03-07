5c72375b085f71762fb12ecb26ef743d3136f0d3
Ester Termens <estertermens@hotmail.com>
2023-03-07 19:25:11 +0100

    cambios

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,21 +1,31 @@
+import { useState } from "react";
+
 const TURNS = {
   X: "x",
   O: "o",
 };
-const board = Array(9).fill(null);
+//creamos otro componente que será el cuadro
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  //Creamos otro estado para saber quien tiene el turno
+  const [turn, setTurn] = useState(TURNS.X);
   return (
     <main className="board">
       <h1>tic tac toe</h1>
       <section className="game">
         {board.map((_, index) => {
           return (
-            <div className="cell" key={index}>
-              <span className="cell__content">{index}</span>
-            </div>
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
           );
         })}
       </section>
+      <section></section>
     </main>
   );
 }
