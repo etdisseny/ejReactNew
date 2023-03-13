c46c76b06f42a8d197f950f6f45f1a17e66c6a69
Ester Termens <estertermens@hotmail.com>
2023-03-13 19:29:54 +0100

    winner

--- 02-tic-tac-toe/src/App.jsx
+++ 02-tic-tac-toe/src/App.jsx
@@ -1,22 +1,69 @@
 import { useState } from "react";
-import {Square} from './components/Square'
+import { Square } from "./components/Square";
 
 const TURNS = {
   X: "x",
   O: "o",
 };
+//convinaciones ganadoras
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 //creamos otro componente que será el cuadro
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   //Creamos otro estado para saber quien tiene el turno empezamos con la X
   const [turn, setTurn] = useState(TURNS.X);
-  
-  //En esta función creamos un newTurn, si el turno es igual a X, el nuevo turno es O, sino X
-  const updateBoard =()=> {
-    const newTurn = turn=== TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn) //actualizamos el estado con el nuevo turno
-  }
+  const [winner, setWinner] = useState(null);
+
+  //recorremos los combos de la constanete WINNER_COMBO
+  const checkWinner = (boardToCheck) => {
+    //revisamos todas las combinaciones ganadoras para ves si X o O ganó
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo; //desestructuramos las posiciones de combo
+      if (
+        //Verificamos la posicion si existe y mirar si el X o O
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] && //miramos si la posicion a y b son iguales es decir hay dos xx o 00
+        boardToCheck[a] === boardToCheck[c] //miramos si es igual qeu la posicion c, en este caso seria tres en ralla
+      ) {
+        return boardToCheck[a]; //nos devuelve si el ganador es X o 0
+      }
+    }
+    //si no hay ganador
+    return null;
+  };
+
+  const updateBoard = (index) => {
+    //no actualizamos la posición si ya tiene algo o ya hay un ganador
+    if (board[index || winner]) return;
+
+    //Actualizar tablero
+    //creamos un newBoard para guardar los nuevos cambios
+    //en el indice del newboard, guardamos el turno y luego actualizamos
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+    //cambiar turno
+    //creamos un newTurn, si el turno es igual a X, el nuevo turno es O, sino X
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn); //actualizamos el estado con el nuevo turno
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }
+  };
 
   return (
     <main className="board">
--- 02-tic-tac-toe/src/components/Square.jsx
+++ 02-tic-tac-toe/src/components/Square.jsx
@@ -1,12 +1,11 @@
-
 export const Square = ({ children, updateBoard, index, isSelected }) => {
-    const className = `square ${isSelected ? "is-selected" : ""}`;
-   const handleClick = ()=>{
-    updateBoard();
-   }
-    return (
-
-        <div onClick={handleClick} className={className}>{children}</div>
-    )
+  const className = `square ${isSelected ? "is-selected" : ""}`;
+  const handleClick = () => {
+    updateBoard(index);
   };
-  
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  );
+};
